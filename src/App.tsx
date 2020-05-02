3ff6211c178803aa7fb78c3c9658ff1a97cc56d4
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-02 02:07:12 +0100

    wraps app in AuthContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Root from './Root';
+
 import ErrorBoundary from './ErrorBoundary';
+import {AuthContextProvider} from './context/AuthContext';
 
 const App: React.FC = () => {
   return (
     <ErrorBoundary>
-      <div className="App">
+      <AuthContextProvider>
         <Root />
-      </div>
+      </AuthContextProvider>
     </ErrorBoundary>
   );
 };
