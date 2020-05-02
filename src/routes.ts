3c04361e6e1a3819715e6ee0b8d7359d97cf4850
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-02 02:09:50 +0100

    adds global routes file

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,13 @@
+import {lazy} from 'react';
+
+const Sample = lazy(() => import('./components/sample/Sample'));
 const LogOut = lazy(() => import('./components/logout/'));
 const Login = lazy(() => import('./pages/login'));
+
+const routes = [
+  {path: '/', component: Sample, ispublic: true},
   {path: '/logout', component: LogOut, ispublic: true},
   {path: '/login', component: Login, ispublic: true},
+];
+
+export default routes;
