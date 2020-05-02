974eebc9c3875c95ead1e276f40ab7362ae39479
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-02 02:12:19 +0100

    adds authReducer

---
+++ src/context/authReducer.ts
@@ -0,0 +1,40 @@
+import {AuthState, Action} from '../types/';
+
+/**========================REDUCER ======================*/
+const reducer = (state: AuthState, action: Action): AuthState => {
+  switch (action.type) {
+    case 'SET_TOKEN':
+      return {
+        ...state,
+        token: action.payload,
+        loggedOut: false,
+      };
+
+    case 'SET_LOADING':
+      return {
+        ...state,
+        loading: action.payload,
+      };
+    case 'SET_ERROR':
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case 'SET_ERROR_MESSAGE':
+      return {
+        ...state,
+        errorMessage: action.payload,
+      };
+
+    case 'LOG_OUT':
+      return {
+        ...state,
+
+        loggedOut: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
