d9184380149d536c00cbf94981dd773cf777b340
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-02 02:09:32 +0100

    creates login route for login components and adds it to app routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,2 +1,4 @@
 const LogOut = lazy(() => import('./components/logout/'));
+const Login = lazy(() => import('./pages/login'));
   {path: '/logout', component: LogOut, ispublic: true},
+  {path: '/login', component: Login, ispublic: true},
