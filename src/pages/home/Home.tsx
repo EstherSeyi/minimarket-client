0d6c6226707c755ff2cec9b56a88af2c8f776b0b
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-06 00:26:29 +0100

    improves home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import {ArrowDownOutlined} from '@ant-design/icons';
+import {Tooltip} from 'antd';
+import {MdLocationOn} from 'react-icons/md';
+import {BsArrowDown} from 'react-icons/bs';
 import {Link} from 'react-scroll';
+import {Link as RouteLink} from 'react-router-dom';
 
 import {Container} from '../../components/container';
+import {FormModal} from './modal';
+import dummy from '../../static/images/dummy.jpg';
 import 'animate.css';
 import './home.css';
 
@@ -16,8 +21,8 @@ const Home: React.FC = () => {
             <h1 className="heading">Mini Market</h1>
             <p className="sub-heading">Search for markets around you</p>
             <p className="action">
-              <button className="btn btn-primary">find a market</button>
-              <button className="btn btn-primary">get nearest market</button>
+              <FormModal btnText="find a market" />
+              <FormModal btnText="get nearest market" />
             </p>
           </main>
           <Link
@@ -28,25 +33,40 @@ const Home: React.FC = () => {
             duration={500}
             delay={0}
             activeClass="arrow-active"
-            className="animated infinite bounce arrow"
+            className="animated infinite heartBeat arrow"
           >
-            <ArrowDownOutlined style={{color: '#fff'}} />
+            <BsArrowDown style={{color: '#fff'}} />
           </Link>
         </Container>
       </section>
       <section>
         <Container>
-          <section id="markets" className="market-grid">
-            <h1 className="title">All Markets</h1>
-            <div className="market"></div>
-            <div className="market"></div>
-            <div className="market"></div>
-            <div className="market"></div>
-            <div className="market"></div>
-            <div className="market"></div>
-            <div className="market"></div>
-            <div className="market"></div>
-            <div className="market"></div>
+          <h1 id="markets" className="markets-title">
+            All Markets
+          </h1>
+          <section className="market-grid">
+            {new Array(10).fill(0).map((_, index) => (
+              <RouteLink key={index} to="#">
+                <div className="market">
+                  <div className="market-image">
+                    <img src={dummy} alt="" />
+                  </div>
+                  <div className="market-body">
+                    <div className="title">Lorem ipsum dolor sit</div>
+                    <div className="market-footer">
+                      <div>Category</div>
+                      <div>
+                        <Tooltip title="Location" trigger={['click', 'hover']}>
+                          <MdLocationOn
+                            style={{fontSize: '1.5rem', color: '#227c9d'}}
+                          />
+                        </Tooltip>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </RouteLink>
+            ))}
           </section>
         </Container>
       </section>
