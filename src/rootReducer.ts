69ec0c2f9cd57fae445ffff659df1693dec24960
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-05 17:25:14 +0100

    adds markets to root reducer

--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import authentication from './redux/reducers/auth.reducer';
 import error from './redux/reducers/error.reducer';
+import markets from './redux/reducers/markets.reducer';
 
 export default combineReducers({
   authentication,
   error,
+  markets,
 });
