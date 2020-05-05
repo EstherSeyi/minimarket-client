81d0abb364842c67b917fa82c14fc919c1411d1a
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-06 00:28:09 +0100

    adds update market action creator

--- src/redux/actions/market.action.ts
+++ src/redux/actions/market.action.ts
@@ -69,12 +69,33 @@ export const createMarket = (data: any) => async (dispatch: AppDispatch) => {
   try {
     dispatch(setLoading(true));
 
-    console.log(data, 'DAATTTTAAAA');
-    const response = await request.post('/market', data);
-
-    console.log(response.data, 'CREATE MARKET');
+    await request.post('/market', data);
 
     dispatch(setLoading(false));
+  } catch (error) {
+    dispatch(setError(true));
+
+    if (error.response) {
+      dispatch(setErrorMessage(error.response.data.message));
+    } else if (error.request) {
+      dispatch(setErrorMessage('Please Try again'));
+    } else {
+      dispatch(setErrorMessage('Process Failed!'));
+    }
+    // console.log(error.config);
+    dispatch(setLoading(false));
+  }
+};
+
+export const updateMarket = (data: any) => async (dispatch: AppDispatch) => {
+  try {
+    dispatch(setLoading(true));
+
+    await request.put('/market', data);
+
+    dispatch(setLoading(false));
+
+    window.location.reload();
   } catch (error) {
     dispatch(setError(true));
 
