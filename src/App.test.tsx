f1bfe327024f3e45269df1693461b8023babc569
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-05 02:19:54 +0100

    modifies tests to suit redux

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import {render} from '@testing-library/react';
+import {Provider} from 'react-redux';
+
 import App from './App';
+import store from './store';
 
 test('renders learn react link', () => {
-  const {container} = render(<App />);
+  const {container} = render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
   expect(container).toMatchInlineSnapshot(`
     <div>
       <div
