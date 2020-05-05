55d5ce12b1e419c9e6bbc54f5357c4b8133c22a2
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-05 02:22:53 +0100

    adds homepage component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,58 @@
 import React from 'react';
+import {ArrowDownOutlined} from '@ant-design/icons';
+import {Link} from 'react-scroll';
 
-const App: React.FC = () => <h1>Home Page</h1>;
+import {Container} from '../../components/container';
+import 'animate.css';
+import './home.css';
 
-export default App;
+const Home: React.FC = () => {
+  return (
+    <>
+      <section className="hero hero-image">
+        <div className="overlay"></div>
+        <Container>
+          <main className="main-section">
+            <h1 className="heading">Mini Market</h1>
+            <p className="sub-heading">Search for markets around you</p>
+            <p className="action">
+              <button className="btn btn-primary">find a market</button>
+              <button className="btn btn-primary">get nearest market</button>
+            </p>
+          </main>
+          <Link
+            to="markets"
+            spy={true}
+            // smooth={true}
+            smooth="easeInOutQuint"
+            offset={-70}
+            duration={500}
+            delay={0}
+            activeClass="arrow-active"
+            className="animated infinite bounce arrow"
+          >
+            <ArrowDownOutlined style={{color: '#fff'}} />
+          </Link>
+        </Container>
+      </section>
+      <section>
+        <Container>
+          <section className="market-grid">
+            <h1 className="title">All Markets</h1>
+            <div className="market"></div>
+            <div className="market"></div>
+            <div className="market"></div>
+            <div className="market"></div>
+            <div className="market"></div>
+            <div className="market"></div>
+            <div className="market"></div>
+            <div className="market"></div>
+            <div className="market"></div>
+          </section>
+        </Container>
+      </section>
+    </>
+  );
+};
+
+export default Home;
