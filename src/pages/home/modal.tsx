9d58beffe54d2ac5555e39cfef8a9897c42dd9c8
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-06 00:26:48 +0100

    adds modal for market searches

---
+++ src/pages/home/modal.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Modal} from 'antd';
+
+import './modal.css';
+
+interface ModalProps extends React.Props<HTMLDivElement> {
+  btnText: string;
+}
+
+export function FormModal(props: ModalProps) {
+  const [open, toggleModal] = React.useState(false);
+
+  const showModal = () => toggleModal(true);
+  const handleOk = () => toggleModal(false);
+  const handleCancel = () => toggleModal(false);
+
+  return (
+    <>
+      <button className="btn hero-btn" onClick={showModal}>
+        {props.btnText}
+      </button>
+      <Modal
+        title="Basic Modal"
+        visible={open}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        centered
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+    </>
+  );
+}
