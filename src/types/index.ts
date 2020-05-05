092e637d8957290f01c50d2a3329d3425354d175
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-05 02:24:30 +0100

    removes redundant types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,3 @@
-export interface AuthState {
-  loading: boolean;
-  error: boolean;
-  token: string;
-  loggedOut: null | boolean;
-  authenticated: null | boolean;
-  errorMessage: string;
-  authError: string;
-}
-
 export type Action = {
   type: string;
   payload: any;
@@ -16,15 +6,4 @@ export type Action = {
 export interface LoginFormValues {
   email: string;
   password: string;
-}
-
-export interface AuthType {
-  auth: AuthState;
-  login: (
-    credentials: LoginFormValues,
-    navigateToDashboard: any,
-  ) => Promise<void>;
-
-  logOut: () => Promise<void>;
-  confirmAuthStatus: () => Promise<void | undefined>;
 }
