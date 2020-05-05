53b808da6cca725faad8dfba1665cbe77a164122
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-06 00:28:36 +0100

    modifies header buttons for control of admin's view display

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
 import {PageHeader, Button} from 'antd';
 
-const App = (props: {handleForm: any; showForm: any}) => {
+const App = (props: {
+  handleDash: any;
+  dashState: any;
+  handleCreate: any;
+  handleSelectionType: any;
+}) => {
+  const {dashState} = props;
+
   return (
     <div className="site-page-header-ghost-wrapper">
       <PageHeader
         ghost={false}
-        onBack={() => props.handleForm()}
-        title={props.showForm ? 'Create Market' : 'Dashboard'}
+        onBack={() => props.handleDash()}
+        title={
+          dashState.create
+            ? 'Create Market'
+            : dashState.edit
+            ? 'Edit Market'
+            : 'Dashboard'
+        }
         subTitle="This is a subtitle"
         extra={[
           <Button
             key="1"
             type="primary"
-            onClick={() => props.handleForm()}
-            disabled={props.showForm}
+            onClick={() => props.handleCreate(true)}
+            disabled={dashState.create || dashState.edit}
           >
             Create Market
           </Button>,
-          <Button key="3">Edit</Button>,
+          <Button
+            key="3"
+            onClick={() => props.handleSelectionType()}
+            disabled={dashState.edit || dashState.create}
+          >
+            Edit
+          </Button>,
           <Button key="2">Logout</Button>,
         ]}
       ></PageHeader>
