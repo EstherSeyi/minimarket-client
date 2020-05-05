0c7c682d082d94f10d40cfd50d3256318b65bd3e
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-05 02:21:39 +0100

    adds Home and View routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,18 @@
 import {lazy} from 'react';
 
-const Sample = lazy(() => import('./components/sample/Sample'));
+const Homepage = lazy(() => import('./pages/home/'));
 const LogOut = lazy(() => import('./components/logout/'));
 const Login = lazy(() => import('./pages/login'));
+const MarketView = lazy(() => import('./pages/market-view'));
 
 const routes = [
-  {path: '/', component: Sample, ispublic: true},
+  //Public Routes
+  {path: '/', component: Homepage, ispublic: true},
   {path: '/logout', component: LogOut, ispublic: true},
   {path: '/login', component: Login, ispublic: true},
+
+  //Private Routes
+  {path: '/view', component: MarketView, ispublic: false},
 ];
 
 export default routes;
