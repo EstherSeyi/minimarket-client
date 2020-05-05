5854ef5ee5f8b5ef56acd2a78de44ca0c7049ea6
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-05 02:22:06 +0100

    substitues local storage for local forage

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,55 +1,19 @@
-import localForage from 'localforage';
-
-export const loadToken = async (): Promise<unknown | undefined> => {
+export const loadState = () => {
   try {
-    const token = await localForage.getItem('token');
-    if (token === null) {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) {
       return undefined;
     }
-
-    return token;
+    return JSON.parse(serializedState);
   } catch (err) {
     return undefined;
   }
 };
 
-export const saveToken = async (token: string): Promise<void> => {
+export const saveState = (state: any) => {
   try {
-    console.log('saving');
-    await localForage.setItem('token', token);
-  } catch {
-    // ignore write errors
-  }
-};
-
-export const deleteFromLocalforage = async (token: string): Promise<void> => {
-  try {
-    await localForage.removeItem(token);
-  } catch (error) {
-    throw new Error('Unable to delete local data');
-  }
-};
-
-export const isUserAuthenticated = () => {
-  return loadToken() !== undefined;
-};
-
-export const loadData = async (item: string): Promise<unknown | undefined> => {
-  try {
-    const data = await localForage.getItem(`${item}`);
-    if (data === null) {
-      return undefined;
-    }
-
-    return data;
-  } catch (err) {
-    return undefined;
-  }
-};
-
-export const saveData = async (name: string, data: any): Promise<void> => {
-  try {
-    await localForage.setItem(`${name}`, data);
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
   } catch {
     // ignore write errors
   }
