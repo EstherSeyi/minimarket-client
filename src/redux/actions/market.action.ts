fd3c5d992be448e50548252883cbaf841772cc33
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-06 17:20:06 +0100

    adds searchMarket action

--- src/redux/actions/market.action.ts
+++ src/redux/actions/market.action.ts
@@ -7,6 +7,10 @@ export const setMarkets = (payload: any): {type: string; payload: any} => ({
   type: 'SET_MARKETS',
   payload,
 });
+export const setSearches = (payload: any): {type: string; payload: any} => ({
+  type: 'SET_SEARCHES',
+  payload,
+});
 
 const marketBoundActionCreator = () => async (dispatch: AppDispatch) => {
   try {
@@ -71,11 +75,11 @@ export const createMarket = (data: any, openNotification: any) => async (
   try {
     dispatch(setLoading(true));
 
-    await request.post('/market', data);
+    const response = await request.post('/market', data);
 
     dispatch(setLoading(false));
 
-    openNotification('bottomRight');
+    openNotification('bottomRight', `${response.data.payload.name} created!`);
   } catch (error) {
     dispatch(setError(true));
 
@@ -100,6 +104,38 @@ export const updateMarket = (data: any) => async (dispatch: AppDispatch) => {
     dispatch(setLoading(false));
 
     window.location.reload();
+  } catch (error) {
+    dispatch(setError(true));
+
+    if (error.response) {
+      dispatch(setErrorMessage(error.response.data.message));
+    } else if (error.request) {
+      dispatch(setErrorMessage('Please Try again'));
+    } else {
+      dispatch(setErrorMessage('Process Failed!'));
+    }
+    // console.log(error.config);
+    dispatch(setLoading(false));
+  }
+};
+
+export const searchMarket = (data: any, navigateToDashboard: any) => async (
+  dispatch: AppDispatch,
+) => {
+  try {
+    dispatch(setLoading(true));
+
+    const response = await request.get('/market/category', {
+      params: {
+        ...data,
+      },
+    });
+
+    dispatch(setSearches(response.data.payload));
+
+    dispatch(setLoading(false));
+
+    navigateToDashboard();
   } catch (error) {
     dispatch(setError(true));
 
