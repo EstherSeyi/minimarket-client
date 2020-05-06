fd2d0c564727f8b86b6bb0904fa9772f6975836e
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-06 17:16:11 +0100

    adds routes for viewing sing market and for viewing search results

--- src/routes.ts
+++ src/routes.ts
@@ -4,12 +4,16 @@ const Homepage = lazy(() => import('./pages/home/'));
 const LogOut = lazy(() => import('./components/logout/'));
 const Login = lazy(() => import('./pages/login'));
 const MarketView = lazy(() => import('./pages/market-view'));
+const SingleMarketVie = lazy(() => import('./pages/single-market'));
+const SearchResult = lazy(() => import('./pages/searches'));
 
 const routes = [
   //Public Routes
   {path: '/', component: Homepage, ispublic: true},
   {path: '/logout', component: LogOut, ispublic: true},
   {path: '/login', component: Login, ispublic: true},
+  {path: '/market', component: SingleMarketVie, ispublic: true},
+  {path: '/search', component: SearchResult, ispublic: true},
 
   //Private Routes
   {path: '/view', component: MarketView, ispublic: false},
