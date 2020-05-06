0a195f8c6e0194f5307997e5e26136a2dc05ed2a
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-06 22:34:38 +0100

    removes redundant dependency and code

--- src/pages/single-market/Single.tsx
+++ src/pages/single-market/Single.tsx
@@ -16,32 +16,7 @@ const App = () => {
         <h1 className="page-title">Market Name</h1>
         <Carousel />
 
-        <div className="main-text">
-          {/* THE MAP */}
-
-          <h2 className="section-title">{"We're really great guys"}</h2>
-          <p>
-            We grew up in downtown Chicago, and we used to play in a band. Jake
-            loves fried chicken, and Elwood loves dry white toast.
-          </p>
-          <p>
-            While the band didnt make it, the hard work we put in trying to
-            promote it sure did. Like a lot of people, we needed a website, and
-            we needed to rank. The experts didnt seem to know what they were
-            talking about, so we figured it out on our own.
-          </p>
-          <h2 className="section-title">We can do all sorts of great stuff</h2>
-          <p>
-            Honestly most people in this industry seem to be full of crap. We
-            dont care what youve heard, or what others have told you, we get
-            results and at the end of the day, what else matters?
-          </p>
-          <p>
-            So sure, maybe were a bit different from what youd expect, but if
-            you want to rank, market, and build your way to the top, were the
-            brothers for you.
-          </p>
-        </div>
+        <div className="main-text">{/* THE MAP */}</div>
       </main>
     </Container>
   );
