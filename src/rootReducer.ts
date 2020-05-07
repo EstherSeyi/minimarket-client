4d7babff49e24ab6a356c612df1af8816c168382
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-07 09:20:05 +0100

    adds market reducer to store

--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux';
 import authentication from './redux/reducers/auth.reducer';
 import error from './redux/reducers/error.reducer';
 import markets from './redux/reducers/markets.reducer';
+import market from './redux/reducers/market.reducer';
 
 export default combineReducers({
   authentication,
   error,
   markets,
+  market,
 });
